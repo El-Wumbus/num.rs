d2bc59f1bfc57ad574e73678ad24ad75889fdd04
el-wumbus <aidanneal4@gmail.com>
2022-09-11 03:07:46 -0400

    Only print ascii when it makes sense

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
 mod functions;
 use functions::*;
 use structopt::StructOpt;
-#[macro_use]
 extern crate prettytable;
 use prettytable::{Table, Row};
 
 fn main()
 {
     let mut table = Table::new();
-    table.add_row(row!["ASCII", "Decimal", "Octal", "Hex", "binary"]);
-    let opt = Opt::from_args();
+    let inputvec: Vec<i64> = parse_args(Opt::from_args());
+    run(&mut table, inputvec);
+    table.printstd();
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "num.rs")]
+struct Opt
+{
+    /// The value, can be hex (0x41), octal (0o101), decimal (65), an ASCII char (A), or a binary number (0b01000001)
+    #[structopt(name = "value")]
+    value: Vec<String>,
+}
+
+fn parse_args(opt: Opt) -> Vec<i64>
+{
     let mut inputvec: Vec<i64> = Vec::new();
     for input in opt.value
     {
@@ -30,11 +43,36 @@ fn main()
             }
         });
     }
+    inputvec
+}
 
+fn run(table: &mut Table, inputvec: Vec<i64>)
+{
+    // First Row
+    let mut first_row_list: Vec<&str> = Vec::new();
+    let mut ascii: bool = false;
+    for input in inputvec.clone()
+    {
+        if input > 32 && input < 127
+        {
+            ascii = true;
+        }
+    }
+
+    if ascii
+    {
+        first_row_list.push("ASCII");
+    }
+
+    first_row_list.append(&mut vec!["Decimal", "Octal", "Hex", "Binary"]);
+    table.add_row(Row::from(first_row_list.clone()));
+
+    // Actual Info
     for inputval in inputvec
     {
         let row: Row;
         let ascii: char;
+
         if inputval < 256
         {
             ascii = inputval as u8 as char;
@@ -43,23 +81,20 @@ fn main()
         {
             ascii = '\0'
         }
-        row = row![
-            format!("{}", ascii),
-            format!("{}", inputval),
-            format!("0{:o}", inputval),
-            format!("0x{:X}", inputval),
-            format!("0b{:b}", inputval)
-        ];
+        let mut rowlist: Vec<String> = Vec::new();
+        for item in first_row_list.clone()
+        {
+            match item
+            {
+                "ASCII" => rowlist.push(format!("{}", ascii)),
+                "Decimal" => rowlist.push(format!("{}", inputval)),
+                "Octal" => rowlist.push(format!("0{:o}", inputval)),
+                "Hex" => rowlist.push(format!("0x{:X}", inputval)),
+                "Binary" => rowlist.push(format!("0b{:b}", inputval)),
+                &_ => (),
+            }
+        }
+        row = Row::from(rowlist);
         table.add_row(row);
     }
-    table.printstd();
-}
-
-#[derive(StructOpt, Debug)]
-#[structopt(name = "num.rs")]
-struct Opt
-{
-    /// The value, can be hex (0x41), octal (0o101), decimal (65), an ASCII char (A), or a binary number (0b01000001)
-    #[structopt(name = "value")]
-    value: Vec<String>,
 }
